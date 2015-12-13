b3f4bcf21b148c4a82595a7fb95c7f6549aa67ba
DetectiveStern <DetectiveStern@users.noreply.github.com>
2015-12-13 17:28:49 -0500

    Adding Arch Logic

--- getDeb.sh
+++ getDeb.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
 site=`wget -qO- https://plex.tv/downloads`
-file=$( echo "$site" | grep 'data-event-label="Ubuntu64"' | grep -Eoi '<a [^>]+>' | grep -Eo 'href="[^\"]+"' | cut -d "\"" -f 2 )
+if [[ `getconf LONG_BIT` == "64" ]]; then
+	echo "Getting 64-bit deb..."
+	file=$( echo "$site" | grep 'data-event-label="Ubuntu64"' | grep -Eoi '<a [^>]+>' | grep -Eo 'href="[^\"]+"' | cut -d "\"" -f 2 )
+else 
+	echo "Getting 32-bit deb..."
+	file=$( echo "$site" | grep 'data-event-label="Ubuntu32"' | grep -Eoi '<a [^>]+>' | grep -Eo 'href="[^\"]+"' | cut -d "\"" -f 2 )
+fi
 wget "$file"
