0972e0b40853b4b0f0cf37b3fb46a51a2a05e13d
DetectiveStern <DetectiveStern@users.noreply.github.com>
2015-12-13 16:44:27 -0500

    Fixed Spacing

--- getDeb.sh
+++ getDeb.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 site=`wget -qO- https://plex.tv/downloads`
-
 file=$( echo "$site" | grep 'data-event-label="Ubuntu64"' | grep -Eoi '<a [^>]+>' | grep -Eo 'href="[^\"]+"' | cut -d "\"" -f 2 )
-
 wget "$file"
