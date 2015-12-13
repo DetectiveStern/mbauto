e94dc6ce306678cfcd14bf15c5c5a4b9560c4995
DetectiveStern <DetectiveStern@users.noreply.github.com>
2015-12-13 13:54:35 -0500

    .....

--- startPlexserver.sh
+++ startPlexserver.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+if [[ -a $HOME/Plex/usr/lib/plexmediaserver/start.sh ]]; then
+echo "Starting Plex Standalone..."
+sleep 3
 $(cd $HOME/Plex/usr/lib/plexmediaserver/ ; screen -d -m -S Plex bash start.sh) 
-
-
+fi
